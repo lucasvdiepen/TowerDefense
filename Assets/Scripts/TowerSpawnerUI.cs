3e76696408899cd92267250b87c6fd5d75988181
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-11-03 00:18:47 +0100

    Added a keybind to open and close tower spawn ui

---
+++ Assets/Scripts/TowerSpawnerUI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSpawnerUI : MonoBehaviour
+{
+    public RectTransform panel;
+
+    public Vector3 openPosition;
+    public Vector3 closePosition;
+
+    public float animationSpeed = 1f;
+
+    private Vector3 panelPosition;
+    private bool isOpen = false;
+
+    public void OpenUI()
+    {
+        panelPosition = openPosition;
+        isOpen = true;
+    }
+
+    public void CloseUI()
+    {
+        panelPosition = closePosition;
+        isOpen = false;
+    }
+
+    public void ToggleUI()
+    {
+        if (isOpen) CloseUI();
+        else OpenUI();
+    }
+
+    private void Start()
+    {
+        panelPosition = closePosition;
+        isOpen = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) ToggleUI();
+
+        panel.anchoredPosition = Vector3.Lerp(panel.anchoredPosition, panelPosition, animationSpeed * Time.deltaTime);
+    }
+}
