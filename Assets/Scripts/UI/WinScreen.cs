817639ba83dbe0b2dcd27f1a3d2bf0bcdede5a48
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-11-05 14:50:39 +0100

    Added win screen and animation

---
+++ Assets/Scripts/UI/WinScreen.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScreen : MonoBehaviour
+{
+    public GameObject winScreenHolder;
+    public Button nextButton;
+    public Button quitButton;
+
+    private void OnEnable()
+    {
+        nextButton.onClick.AddListener(NextButtonClicked);
+        quitButton.onClick.AddListener(QuitButtonClicked);
+    }
+
+    private void OnDisable()
+    {
+        nextButton.onClick.RemoveAllListeners();
+        quitButton.onClick.RemoveAllListeners();
+    }
+
+    public void ShowLoseScreen()
+    {
+        FindObjectOfType<PauseScreen>().GameEnded();
+        FindObjectOfType<EnemySpawner>().StopSpawning();
+
+        winScreenHolder.SetActive(true);
+
+    }
+
+    public void NextButtonClicked()
+    {
+        Debug.Log("Load next level here");
+    }
+
+    public void QuitButtonClicked()
+    {
+        SceneManager.LoadScene("MainScreen");
+    }
+}
