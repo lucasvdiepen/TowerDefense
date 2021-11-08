f13ade44d1ec2afcfba9c390c21ec83fad6e97af
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-11-08 11:07:22 +0100

    Added new level loader in game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public TextAsset[] jsonMapData;
+    public int wavesToWinIncrease = 3;
 
-    public void NewWaveStarted(int wave)
+    public static GameManager gameManager;
+
+    private void Awake()
     {
+        if (gameManager != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameManager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
+    public void LoadNewLevel()
+    {
+        FindObjectOfType<GameInfo>().SetMapData(jsonMapData[Random.Range(0, jsonMapData.Length)]);
+        FindObjectOfType<GameInfo>().SetWavesToFinish(GetWavesToWin() + wavesToWinIncrease);
+
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public int GetWavesToWin()
+    {
+        return PlayerPrefs.GetInt("wavesToWin", 3);
     }
 }
