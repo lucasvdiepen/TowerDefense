8aec72522d6eb4db3557374967741ad211ecdda8
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-11-09 00:06:15 +0100

    Added arrow button to tower spawner ui

--- Assets/Scripts/TowerSpawnerUI.cs
+++ Assets/Scripts/TowerSpawnerUI.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerSpawnerUI : MonoBehaviour
 {
     public RectTransform panel;
+    public RectTransform arrow;
+
+    public Button arrowButton;
 
     public Vector3 openPosition;
     public Vector3 closePosition;
@@ -14,15 +18,32 @@ public class TowerSpawnerUI : MonoBehaviour
     private Vector3 panelPosition;
     private bool isOpen = false;
 
+    private float arrowOpenRotation = 0f;
+    private float arrowCloseRotation = 180f;
+
+    private float arrowRotation = 0;
+
+    private void OnEnable()
+    {
+        arrowButton.onClick.AddListener(ToggleUI);   
+    }
+
+    private void OnDisable()
+    {
+        arrowButton.onClick.RemoveAllListeners();
+    }
+
     public void OpenUI()
     {
         panelPosition = openPosition;
+        arrowRotation = arrowCloseRotation;
         isOpen = true;
     }
 
     public void CloseUI()
     {
         panelPosition = closePosition;
+        arrowRotation = arrowOpenRotation;
         isOpen = false;
     }
 
@@ -34,8 +55,7 @@ public class TowerSpawnerUI : MonoBehaviour
 
     private void Start()
     {
-        panelPosition = closePosition;
-        isOpen = false;
+        OpenUI();
     }
 
     private void Update()
@@ -43,5 +63,6 @@ public class TowerSpawnerUI : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F)) ToggleUI();
 
         panel.anchoredPosition = Vector3.Lerp(panel.anchoredPosition, panelPosition, animationSpeed * Time.deltaTime);
+        arrow.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Lerp(arrow.eulerAngles.z, arrowRotation, animationSpeed * Time.deltaTime)));
     }
 }
